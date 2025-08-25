94255b2a4cd3801c86f4fa9b5f0b0fa73dc787e5
Iversen <iarroyou@uni.pe>
2025-08-25 10:29:57 -0500

    fdgdfgfdg

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
-import { motion, AnimatePresence, LazyMotion, domAnimation } from 'framer-motion';
-import './App.css';
-import { Rutas } from './routing/Rutas';
-import { Header } from './components/layout/Header';
+import { useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  LazyMotion,
+  domAnimation,
+} from "framer-motion";
+import "./App.css";
+import { Rutas } from "./routing/rutas";
+import { Header } from "./components/layout/Header";
 
 function App() {
- 
-
   return (
     <>
-    
-      <Rutas/>
+      <Rutas />
     </>
   );
 }
